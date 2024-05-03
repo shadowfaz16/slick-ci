23021a9b113a2483eafad32c1b639395e4bfb4dc
Shadow <erikfazju@gmail.com>
2024-05-03 22:05:57 +1000

    initial screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,29 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          100: '#0D6E6E',
+          200: '#4a9d9c',
+          300: '#afffff'
+        },
+        accent: {
+          100: '#FF3D3D',
+          200: '#ffe0c8'
+        },
+        text: {
+          100: '#FFFFFF',
+          200: '#e0e0e0'
+        },
+        bg: {
+          100: '#0D1F2D',
+          200: '#1d2e3d',
+          300: '#354656'
+        }
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
+      }
     },
   },
   plugins: [],
